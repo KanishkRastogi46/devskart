5e9b5fcc6570636ae57976cb98304529509da89b
KanishkRastogi46 <113916868+KanishkRastogi46@users.noreply.github.com>
2025-04-15 04:10:32 +0530

    showing dynamic items in store and home

--- store/urls.py
+++ store/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from store.views import *
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("", index, name="store"),
     path("<slug:category_name>/", index, name="product_by_category"),
+    path("<slug:category_name>/<slug:product_name>/", product_page, name="product_page_with_category"),
+    path("<slug:product_name>/", product_page, name="product_page")
 ]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from store.models import Product
 from category.models import Category
+from store.models import Product
 
 # Create your views here.
 def index(request, category_name=None):
@@ -14,5 +15,10 @@ def index(request, category_name=None):
             pass
     else:     
         products = Product.objects.all().filter(is_available=True)
-    context = {'products': products, 'count': products.count() if products else 0}
+    context = {'products': products, 'category': category_name, 'count': products.count() if products else 0}
     return render(request, 'store/store.html', context)
+
+
+def product_page(request, category_name=None, product_name=None):
+    product = get_object_or_404(Product, slug=product_name, category__slug=category_name) if category_name else get_object_or_404(Product, slug=product_name)
+    return render(request, 'store/product_page.html', {'product': product})
